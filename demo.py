f6b285a9359b59c0e21abb4700580181896d3099
Fan-Huo <chen_alphonse_xuanli@outlook.com>
2023-10-12 16:17:54 +0800

    added kaleido as required

--- demo.py
+++ demo.py
@@ -1,7 +1,5 @@
-# 1
 import tqdm
-
-tqdm.tqdm = tqdm.notebook.tqdm  # notebook-friendly progress bars
+import shutil
 from pathlib import Path
 import numpy as np
 
@@ -12,14 +10,18 @@ from hloc.utils import viz_3d
 # # Setup
 # Here we define some output paths.
 
-images = Path('datasets/sacre_coeur')
-outputs = Path('outputs/demo/')
-!rm - rf $outputs
+images = Path('datasets/sacre_coeur').resolve()
+outputs = Path('outputs/demo/').resolve()
+if outputs.exists():
+    shutil.rmtree(outputs)
 sfm_pairs = outputs / 'pairs-sfm.txt'
 loc_pairs = outputs / 'pairs-loc.txt'
 sfm_dir = outputs / 'sfm'
 features = outputs / 'features.h5'
 matches = outputs / 'matches.h5'
+# sfm_dir.mkdir(parents=True, exist_ok=True)
+# features.mkdir(parents=True, exist_ok=True)
+# matches.mkdir(parents=True, exist_ok=True)
 
 feature_conf = extract_features.confs['disk']
 matcher_conf = match_features.confs['disk+lightglue']
@@ -29,6 +31,10 @@ matcher_conf = match_features.confs['disk+lightglue']
 references = [p.relative_to(images).as_posix() for p in (images / 'mapping/').iterdir()]
 print(len(references), "mapping images")
 plot_images([read_image(images / r) for r in references], dpi=25)
+
+extract_features.main(feature_conf, images, image_list=references, feature_path=features)
+pairs_from_exhaustive.main(sfm_pairs, image_list=references)
+match_features.main(matcher_conf, sfm_pairs, features=features, matches=matches)
 
 # Then we extract features and match them across image pairs. Since we deal with few images, we simply match all pairs
 # exhaustively. For larger scenes, we would use image retrieval, as demonstrated in the other notebooks.
@@ -48,7 +54,14 @@ url = "https://upload.wikimedia.org/wikipedia/commons/5/53/Paris_-_Basilique_du_
 # url = "https://upload.wikimedia.org/wikipedia/commons/5/59/Basilique_du_Sacr%C3%A9-C%C5%93ur_%285430392880%29.jpg"
 # url = "https://upload.wikimedia.org/wikipedia/commons/8/8e/Sacr%C3%A9_C%C5%93ur_at_night%21_%285865355326%29.jpg"
 query = 'query/night.jpg'
-!mkdir -p $images/query && wget $url -O $images/$query -q
+# !mkdir -p $images/query && wget $url -O $images/$query -q
+
+import urllib.request
+dp_query = Path(f"{images}/query").resolve()
+dp_query.mkdir(exist_ok=True, parents=True)
+# Assuming variables url, images and query are defined
+urllib.request.urlretrieve(url, f"{images}/{query}")
+
 plot_images([read_image(images / query)], dpi=75)
 
 # Again, we extract features for the query and match them exhaustively.
