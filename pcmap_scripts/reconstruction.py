8fb549d4c2aa9802e5103c14b8bb458308a70cfc
Fan-Huo <chen_alphonse_xuanli@outlook.com>
2023-10-13 17:13:37 +0800

    careful use the change name out of the file ...

---
+++ pcmap_scripts/reconstruction.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import pycolmap
+
+if __name__ == '__main__':
+    output_path = Path("../outputs").resolve() / "colmap_sfm_4_run_oct_9_tot_300"
+    image_dir = Path("../datasets/sfm_4_run_oct_9_tot_300/mapping").resolve()
+
+    output_path.mkdir()
+    mvs_path = output_path / "mvs"
+    database_path = output_path / "database.db"
+
+    pycolmap.extract_features(database_path, image_dir)
+    pycolmap.match_exhaustive(database_path)
+    maps = pycolmap.incremental_mapping(database_path, image_dir, output_path)
+    maps[0].write(output_path)
+    # dense reconstruction
+    pycolmap.undistort_images(mvs_path, output_path, image_dir)
+    pycolmap.patch_match_stereo(mvs_path)  # requires compilation with CUDA
+    pycolmap.stereo_fusion(mvs_path / "dense.ply", mvs_path)
